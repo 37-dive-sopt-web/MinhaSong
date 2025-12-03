865e26069ab6b9f0b27b097c115cad1bd3baf40e
민하 <minha@minhaui-noteubug.local>
2025-12-03 15:37:17 +0900

    refactor: selected가 아니라 value로 선택 상태 제어하도록 개선

--- Week3/Homework/card-match-game/src/components/Game/Game.jsx
+++ Week3/Homework/card-match-game/src/components/Game/Game.jsx
@@ -72,8 +72,8 @@ export default function Game() {
         </G.CardWrapper>
       </G.GameBoard>
       <G.GameStatus>
-        <G.Select onChange={(e) => handleSelectLevel(e)}>
-          <option value="1" selected>Level 1</option>
+        <G.Select value={selectedLevel.level} onChange={(e) => handleSelectLevel(e)}>
+          <option value="1">Level 1</option>
           <option value="2">Level 2</option>
           <option value="3">Level 3</option>
         </G.Select>
